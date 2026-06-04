84d498fdf517176adce268d6a99b00e62ee041ed
Kartik <kartik3344@gmail.com>
2026-06-04 18:18:29 +0530

    added UserBalances type alias

--- backend/engine/src/store/store.ts
+++ backend/engine/src/store/store.ts
@@ -1,6 +1,7 @@
 import { OrderBook, Order, Fill, Balance } from "../types"
 
-export const BALANCES = new Map<string, Record<string, Balance>>();
+type UserBalances = Record<string, Balance>
+export const BALANCES = new Map<string, UserBalances>();
 export const ORDERBOOK = new Map<string, OrderBook>();
 export const ORDERS = new Map<string, Order>();
 export const FILLS: Fill[] = []
