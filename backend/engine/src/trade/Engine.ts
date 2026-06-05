e5a653690cb7cbceb9984f6ac7cf87272c72cd78
Kartik <kartik3344@gmail.com>
2026-06-05 18:02:59 +0530

    implement balance settlement after trade execution

--- backend/engine/src/trade/Engine.ts
+++ backend/engine/src/trade/Engine.ts
@@ -1,5 +1,6 @@
-import { BALANCES } from "../store/store";
-import { Order } from "../types";
+import { BALANCES, ORDERBOOK } from "../store/store";
+import { Fill, Order } from "../types";
+import { placeOrder } from "./orderbook";
 
 export function createOrder(order:Order){
     const { market, price, quantity, userId, side } = order;
@@ -7,7 +8,11 @@ export function createOrder(order:Order){
     const baseAsset = market.split("_")[0];
     const quoteAsset = market.split("_")[1];
     
-    if(!BALANCES.get(userId)) throw new Error("user does not exist")
+    if(!BALANCES.get(userId)) throw new Error("user does not exist");
+
+    if(!ORDERBOOK.get(market)) throw new Error("Market does not exist");
+
+
 
 }
 
@@ -41,7 +46,32 @@ function checkAndLockFunds(quoteAsset:string, baseAsset: string, userId:string, 
         
     }
 }
+function updateBalances(userId:string, fills:Fill[], baseAsset:string, quoteAsset:string, side:"buy"| "sell"){
+    //baseAsset = TATA, quoteAsset = INR
+    if(side === "buy"){
+        fills.forEach(fill => {
+            // update baseAsset balance
+            BALANCES.get(userId)![baseAsset].available += fill.qty;
 
-function updateBalances(order:Order){
+            BALANCES.get(fill.otherUserId)![baseAsset].locked -= fill.qty;
 
+            //update quoteAsset balance
+
+            BALANCES.get(userId)![quoteAsset].locked -= fill.price * fill.qty;
+
+            BALANCES.get(fill.otherUserId)![quoteAsset].available += fill.price * fill.qty;
+        })
+    } else {
+        fills.forEach(fill => {
+        // update baseAsset balance
+        BALANCES.get(userId)![baseAsset].locked -= fill.qty;
+        BALANCES.get(fill.otherUserId)![baseAsset].available += fill.qty;
+
+        //update quoteAsset balance
+
+        BALANCES.get(userId)![quoteAsset].available += fill.price * fill.qty;
+        BALANCES.get(fill.otherUserId)![quoteAsset].locked -= fill.price * fill.qty;
+        })
+        
+    }
 }
