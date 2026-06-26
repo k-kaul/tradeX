db18fbe431872059191381ec3e10cd06798b1731
Kartik <kartik3344@gmail.com>
2026-06-26 20:38:15 +0530

    createOrder func complete

--- backend/engine/src/trade/Engine.ts
+++ backend/engine/src/trade/Engine.ts
@@ -1,19 +1,43 @@
 import { BALANCES, ORDERBOOK } from "../store/store";
 import { Fill, Order } from "../types";
-import { placeOrder } from "./orderbook";
+import { matchBid, placeOrder } from "./orderbook";
 
-export function createOrder(order:Order){
-    const { market, price, quantity, userId, side } = order;
-
+export function createOrder(market:string, price:number, quantity:number, userId:string, side:"buy"| "sell"){
+    
     const baseAsset = market.split("_")[0];
     const quoteAsset = market.split("_")[1];
+
+    const orderbook = ORDERBOOK.get(market);
+    const user = BALANCES.get(userId)!.userId;
     
     if(!BALANCES.get(userId)) throw new Error("user does not exist");
 
-    if(!ORDERBOOK.get(market)) throw new Error("Market does not exist");
+    if(!orderbook) throw new Error("Market does not exist");
 
+    if(!(user.available > price * quantity)) throw new Error("Insufficient Balance");
+    
+    checkAndLockFunds(quoteAsset, baseAsset, userId, quantity, price, side);    
 
+    const order: Order = {
+        orderId:Math.random().toString(36),
+        market,
+        price,
+        quantity,
+        userId,
+        filled:0,
+        side
+    }
 
+    const {fills, executedQuantity} = placeOrder(order);
+
+    updateBalances(userId, fills, baseAsset, quoteAsset, side);
+    // Todo: free locked funds
+
+    return {
+        fills,
+        executedQuantity,
+        orderId: order.orderId
+    }
 }
 
 function checkAndLockFunds(quoteAsset:string, baseAsset: string, userId:string, quantity:number, price:number, side:"buy" | "sell"){
@@ -42,12 +66,13 @@ function checkAndLockFunds(quoteAsset:string, baseAsset: string, userId:string, 
 
         userBaseAssetBalance.available = userBaseAssetBalance.available -  quantity;
 
-        userBaseAssetBalance.locked = userBaseAssetBalance.locked +  quantity;
-        
+        userBaseAssetBalance.locked = userBaseAssetBalance.locked +  quantity;      
     }
 }
 function updateBalances(userId:string, fills:Fill[], baseAsset:string, quoteAsset:string, side:"buy"| "sell"){
     //baseAsset = TATA, quoteAsset = INR
+    let fillCounter:number = 0;
+    let otherUserId = "";
     if(side === "buy"){
         fills.forEach(fill => {
             // update baseAsset balance
@@ -60,6 +85,7 @@ function updateBalances(userId:string, fills:Fill[], baseAsset:string, quoteAsse
             BALANCES.get(userId)![quoteAsset].locked -= fill.price * fill.qty;
 
             BALANCES.get(fill.otherUserId)![quoteAsset].available += fill.price * fill.qty;
+            fillCounter += 1
         })
     } else {
         fills.forEach(fill => {
@@ -70,8 +96,12 @@ function updateBalances(userId:string, fills:Fill[], baseAsset:string, quoteAsse
         //update quoteAsset balance
 
         BALANCES.get(userId)![quoteAsset].available += fill.price * fill.qty;
-        BALANCES.get(fill.otherUserId)![quoteAsset].locked -= fill.price * fill.qty;
-        })
+        BALANCES.get(fill.otherUserId)![baseAsset].locked -= fill.price * fill.qty;
         
+        fillCounter += 1
+        
+        })
     }
 }
+
+// TODO: free locked funds function
