6b051a85e3a0bf909ded44373a15d636bd5bb3dc
Kartik <kartik3344@gmail.com>
2026-07-09 20:41:36 +0530

    add types for api to engine messages

--- backend/engine/src/types.ts
+++ backend/engine/src/types.ts
@@ -33,3 +33,50 @@ export interface Fill {
   createdAt: number;
   otherUserId:string;
 }
+
+// messages sent from api to engine
+interface CreateOrderMessage{
+    type: "CREATE_ORDER";
+    data: {
+        market: string;
+        price: number;
+        quantity: number;
+        userId: string;
+        side: "buy" | "sell";       
+    }
+}
+
+interface CancelOrderMessage {
+    type: "CANCEL_ORDER";
+    data: {
+        orderId:string;
+        market:string;
+    }
+}
+
+interface OnRampMessage {
+    type: "ON_RAMP";
+    data: {
+        userId:string;
+        amount:number;
+    }
+}
+
+interface GetDepthMessage {
+    type: "GET_DEPTH";
+    data: {
+        market: string;
+    }
+}
+
+interface GetOpenOrdersMessage {
+    type: "GET_OPEN_ORDERS";
+    data: {
+        userId: string;
+    }
+}
+
+export type MessagesFromApi = CreateOrderMessage | CancelOrderMessage | OnRampMessage | GetDepthMessage | GetOpenOrdersMessage;
+// end
+
+// messages sent from engine back to Api
